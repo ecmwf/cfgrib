5c82e0f365b28f0c437a5de5735e2c71afebeda6
Alessandro Amici <a.amici@bopen.eu>
2018-06-01 17:01:01 +0200

    isinstance for python2/3

--- eccodes_grib/eccodes.py
+++ eccodes_grib/eccodes.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-from builtins import int, float, bytes
+from builtins import bytes, float, int, isinstance
 
 import functools
 import logging
