12d7d78bcc992c7e580c9ac5523dd747a60adb77
Alessandro Amici <a.amici@bopen.eu>
2018-06-02 10:31:30 +0200

    Sync CDS ERA5 tests.

--- tests/cds_test_10_era5.py
+++ tests/cds_test_10_era5.py
@@ -6,47 +6,83 @@ import eccodes_grib
 import cdscommon
 
 
-REQUESTS = {
-    'reanalysis-era5-single-levels': {
-        'variable': '2m_temperature',
-        'product_type': 'reanalysis',
-        'year': '2017',
-        'month': '01',
-        'day': '01',
-        'time': ['00:00', '12:00'],
-        'grid': [3, 3],
-        'format': 'grib',
-    },
-    'reanalysis-era5-pressure-levels': {
-        'variable': 'temperature',
-        'pressure_level': ['500', '850'],
-        'product_type': 'ensemble_members',
-        'year': '2017',
-        'month': '01',
-        'day': '01',
-        'time': ['00:00', '12:00'],
-        'grid': [3, 3],
-        'format': 'grib',
-    },
+TEST_FILES = {
+    'era5-single-levels-reanalysis': [
+        'reanalysis-era5-single-levels',
+        {
+            'variable': '2m_temperature',
+            'product_type': 'reanalysis',
+            'year': '2017',
+            'month': '01',
+            'day': '01',
+            'time': ['00:00', '12:00'],
+            'grid': ['3', '3'],
+            'format': 'grib',
+        },
+        192,
+    ],
+    'era5-single-levels-ensemble_members': [
+        'reanalysis-era5-single-levels',
+        {
+            'variable': '2m_temperature',
+            'product_type': 'ensemble_members',
+            'year': '2017',
+            'month': '01',
+            'day': '01',
+            'time': ['00:00', '12:00'],
+            'grid': ['3', '3'],
+            'format': 'grib',
+        },
+        192,
+    ],
+    'era5-pressure-levels-reanalysis': [
+        'reanalysis-era5-pressure-levels',
+        {
+            'variable': 'temperature',
+            'pressure_level': ['500', '850'],
+            'product_type': 'reanalysis',
+            'year': '2017',
+            'month': '01',
+            'day': '01',
+            'time': ['00:00', '12:00'],
+            'grid': ['3', '3'],
+            'format': 'grib',
+        },
+        192,
+    ],
+    'era5-pressure-levels-ensemble_members': [
+        'reanalysis-era5-pressure-levels',
+        {
+            'variable': 'temperature',
+            'pressure_level': ['500', '850'],
+            'product_type': 'ensemble_members',
+            'year': '2017',
+            'month': '01',
+            'day': '01',
+            'time': ['00:00', '12:00'],
+            'grid': ['3', '3'],
+            'format': 'grib',
+        },
+        192,
+    ],
 }
-EUROPE_EXTENT = {'latitude': slice(65, 30), 'longitude': slice(0, 40)}
 
 
-@pytest.mark.parametrize('dataset', REQUESTS.keys())
-def test_reanalysis_Stream(dataset):
-    request = REQUESTS[dataset]
-    path = cdscommon.ensure_data(dataset, request)
+@pytest.mark.parametrize('test_file', TEST_FILES.keys())
+def test_reanalysis_Stream(test_file):
+    dataset, request, key_count = TEST_FILES[test_file]
+    path = cdscommon.ensure_data(dataset, request, name='cds-' + test_file + '-{uuid}.grib')
 
     stream = eccodes_grib.Stream(path)
     leader = stream.first()
-    assert len(leader) in (191, 192)
+    assert len(leader) == key_count
     assert sum(1 for _ in stream) == leader['count']
 
 
-@pytest.mark.parametrize('dataset', REQUESTS.keys())
-def test_reanalysis_Dataset(dataset):
-    request = REQUESTS[dataset]
-    path = cdscommon.ensure_data(dataset, request)
+@pytest.mark.parametrize('test_file', TEST_FILES.keys())
+def test_reanalysis_Dataset(test_file):
+    dataset, request, key_count = TEST_FILES[test_file]
+    path = cdscommon.ensure_data(dataset, request, name='cds-' + test_file + '-{uuid}.grib')
 
     res = eccodes_grib.Dataset(path)
     assert len(res.variables) == 1
