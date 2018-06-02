68eb1bd58b84c1937f6f8d15bb9ea9f02a402e22
Alessandro Amici <a.amici@bopen.eu>
2018-06-02 19:42:12 +0200

    Drop impossible to get right code.

--- tests/cdscommon.py
+++ tests/cdscommon.py
@@ -25,21 +25,3 @@ def ensure_data(dataset, request, folder=SAMPLE_DATA_FOLDER, name='{uuid}.grib')
             os.unlink(path + '.tmp')
             raise
     return path
-
-
-def message_count(dataset, request, count=1):
-    if dataset == 'reanalysis-era5-single-levels' \
-            and request.get('product_type') == 'ensemble_members':
-        count = 20
-    elif dataset == 'reanalysis-era5-pressure-levels' \
-            and request.get('product_type') == 'ensemble_members':
-        count = 20
-    elif dataset == 'seasonal-original-single-levels':
-        count = 51
-    elif dataset.startswith('seasonal-') and request.get('product_type') == 'monthly_mean':
-        count = 51
-    for key in ['year', 'month', 'day', 'time', 'leadtime_hour', 'leadtime_month']:
-        value = request.get(key)
-        if isinstance(value, list):
-            count *= len(value)
-    return count
