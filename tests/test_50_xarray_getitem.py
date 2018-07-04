f580fe7b869b63b46e37d7d94a532d8bc1fb3bc9
Alessandro Amici <a.amici@bopen.eu>
2018-07-04 23:29:34 +0200

    Add some slice tests.

--- tests/test_50_xarray_getitem.py
+++ tests/test_50_xarray_getitem.py
@@ -13,3 +13,6 @@ def test_all():
     da = xarray_store.open_dataset(TEST_DATA).data_vars['t']
 
     assert np.allclose(da.mean(), 262.92133)
+    assert np.allclose(da.sel(level=50000).mean(), 252.22284)
+    assert np.allclose(da.sel(number=slice(2, 5)).mean(), 262.92184)
+    assert np.allclose(da.sel(latitude=slice(90, 40), longitude=slice(0, 33)).mean(), 250.58322)
