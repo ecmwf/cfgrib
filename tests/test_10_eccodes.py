3b163de45c4b5615d9d860abfc55b5a4186d6125
Alessandro Amici <a.amici@bopen.eu>
2018-07-23 18:01:53 +0200

    Fix Python 2 tests and keep import ordered.

--- tests/test_10_eccodes.py
+++ tests/test_10_eccodes.py
@@ -1,6 +1,6 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-from builtins import int, float, bytes
+from builtins import bytes, float, int, str
 
 import os.path
 
