497e7e87df624172e0e92255f4deaf9a1f6e577a
Alessandro Amici <a.amici@bopen.eu>
2018-11-04 23:28:12 +0100

    Reach 100% coverage for cfunits.py.

--- tests/test_10_cfunits.py
+++ tests/test_10_cfunits.py
@@ -5,5 +5,6 @@ from cf2cdm import cfunits
 
 
 def test_are_convertible():
+    assert cfunits.are_convertible('m', 'm')
     assert cfunits.are_convertible('hPa', 'Pa')
     assert not cfunits.are_convertible('m', 'Pa')
