8d69df47f01dd7500d94b6b3d353242b0b717aa8
Alessandro Amici <a.amici@bopen.eu>
2018-11-06 13:02:16 +0100

    Split test per files.

---
+++ tests/test_35_cfgrib_.py
@@ -0,0 +1,29 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os.path
+
+import xarray as xr
+
+from cfgrib import cfgrib_
+
+SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'sample-data')
+TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
+
+
+def test_CfGribDataStore():
+    datastore = cfgrib_.CfGribDataStore(TEST_DATA, encode_cf=())
+    expected = {'number': 10, 'dataDate': 2, 'dataTime': 2, 'level': 2, 'values': 7320}
+    assert datastore.get_dimensions() == expected
+
+
+def test_xarray_open_dataset():
+    datastore = cfgrib_.CfGribDataStore(TEST_DATA, encode_cf=(), lock=cfgrib_.SerializableLock())
+    res = xr.open_dataset(datastore)
+
+    assert res.attrs['GRIB_edition'] == 1
+    assert res['t'].attrs['GRIB_gridType'] == 'regular_ll'
+    assert res['t'].attrs['GRIB_units'] == 'K'
+    assert res['t'].dims == ('number', 'dataDate', 'dataTime', 'level', 'values')
+
+    assert res['t'].mean() > 0.
--- tests/test_40_xarray_store.py
+++ tests/test_40_xarray_store.py
@@ -4,33 +4,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os.path
 
 import pytest
-import xarray as xr
 
 from cfgrib import eccodes
-from cfgrib import cfgrib_
 from cfgrib import xarray_store
 
 SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'sample-data')
 TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
 TEST_CORRUPTED = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-corrupted.grib')
-
-
-def test_CfGribDataStore():
-    datastore = cfgrib_.CfGribDataStore(TEST_DATA, encode_cf=())
-    expected = {'number': 10, 'dataDate': 2, 'dataTime': 2, 'level': 2, 'values': 7320}
-    assert datastore.get_dimensions() == expected
-
-
-def test_xarray_open_dataset():
-    datastore = cfgrib_.CfGribDataStore(TEST_DATA, encode_cf=())
-    res = xr.open_dataset(datastore)
-
-    assert res.attrs['GRIB_edition'] == 1
-    assert res['t'].attrs['GRIB_gridType'] == 'regular_ll'
-    assert res['t'].attrs['GRIB_units'] == 'K'
-    assert res['t'].dims == ('number', 'dataDate', 'dataTime', 'level', 'values')
-
-    assert res['t'].mean() > 0.
 
 
 def test_open_dataset():
