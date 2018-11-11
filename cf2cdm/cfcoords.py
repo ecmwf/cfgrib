0613867a5e993ba4b33533b9b1c26e3a56c38d5e
Alessandro Amici <a.amici@bopen.eu>
2018-11-11 14:03:37 +0100

    Add authors.

--- cf2cdm/cfcoords.py
+++ cf2cdm/cfcoords.py
@@ -12,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Authors:
+#   Alessandro Amici - B-Open - https://bopen.eu
+#
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
