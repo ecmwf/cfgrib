e94d133563c8a4476b050aa66a03d9bc10d24e80
Alessandro Amici <a.amici@bopen.eu>
2018-12-27 14:17:49 +0100

    Fix docs and CLI tests.

--- tests/test_20_main.py
+++ tests/test_20_main.py
@@ -1,14 +1,17 @@
 
+import click.testing
 import pytest
 
 from cfgrib import __main__
 
 
-def test_main(capsys):
-    __main__.main(argv=['selfcheck'])
-    stdout, _ = capsys.readouterr()
+def test_main():
+    runner = click.testing.CliRunner()
 
-    assert 'Your system is ready.' in stdout
+    res = runner.invoke(__main__.cfgrib_cli, ['selfcheck'])
 
-    with pytest.raises(RuntimeError):
-        __main__.main(argv=['non-existent-command'])
+    assert res.exit_code == 0
+    assert 'Your system is ready.' in res.output
+
+    res = runner.invoke(__main__.cfgrib_cli, ['non-existent-command'])
+    assert res.exit_code == 2
