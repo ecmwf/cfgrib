7769e54c5dcc8410908f01ca1bef646c7be2bc57
Alessandro Amici <a.amici@bopen.eu>
2018-12-27 12:07:58 +0100

    Add tests for #41.

--- tests/test_50_datamodels.py
+++ tests/test_50_datamodels.py
@@ -9,20 +9,45 @@ from cf2cdm import datamodels
 from cfgrib import xarray_store
 
 SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'sample-data')
-TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
+TEST_DATA1 = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
+TEST_DATA2 = os.path.join(SAMPLE_DATA_FOLDER, 'lambert_grid.grib')
 
 
 def test_cds():
-    ds = xarray_store.open_dataset(TEST_DATA)
+    ds = xarray_store.open_dataset(TEST_DATA1)
 
     res = cfcoords.translate_coords(ds, coord_model=datamodels.CDS)
 
     assert set(res.dims) == {'forecast_reference_time', 'lat', 'lon', 'plev', 'realization'}
+    assert set(res.coords) == {
+        'forecast_reference_time', 'lat', 'leadtime', 'lon', 'plev', 'realization', 'time',
+    }
+
+    ds = xarray_store.open_dataset(TEST_DATA2)
+
+    res = cfcoords.translate_coords(ds, coord_model=datamodels.CDS)
+
+    assert set(res.dims) == {'x', 'y'}
+    assert set(res.coords) == {
+        'forecast_reference_time', 'heightAboveGround', 'lat', 'leadtime', 'lon', 'time',
+    }
 
 
 def test_ecmwf():
-    ds = xarray_store.open_dataset(TEST_DATA)
+    ds = xarray_store.open_dataset(TEST_DATA1)
 
     res = cfcoords.translate_coords(ds, coord_model=datamodels.ECMWF)
 
-    assert set(res.dims) == {'time', 'latitude', 'longitude', 'level', 'number'}
+    assert set(res.dims) == {'latitude', 'level', 'longitude', 'number', 'time'}
+    assert set(res.coords) ==  {
+        'latitude', 'level', 'longitude', 'number', 'step', 'time', 'valid_time',
+    }
+
+    ds = xarray_store.open_dataset(TEST_DATA2)
+
+    res = cfcoords.translate_coords(ds, coord_model=datamodels.ECMWF)
+
+    assert set(res.dims) == {'x', 'y'}
+    assert set(res.coords) == {
+        'heightAboveGround', 'latitude', 'longitude', 'step', 'time', 'valid_time',
+    }
