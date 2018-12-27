9a571e267b5818de132123190698d5f3ea443071
Alessandro Amici <a.amici@bopen.eu>
2018-12-27 18:46:13 +0100

    Get back __main__ to 100% coverage.

--- tests/test_20_main_to_netcdf.py
+++ tests/test_20_main_to_netcdf.py
@@ -13,7 +13,7 @@ SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'sample-data')
 TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
 
 
-def test_cfgrib_cli_to_netcdf():
+def test_cfgrib_cli_to_netcdf(tmpdir):
     runner = click.testing.CliRunner()
 
     res = runner.invoke(__main__.cfgrib_cli, ['to_netcdf'])
@@ -22,6 +22,12 @@ def test_cfgrib_cli_to_netcdf():
     assert res.output == ''
 
     res = runner.invoke(__main__.cfgrib_cli, ['to_netcdf', TEST_DATA])
+
+    assert res.exit_code == 0
+    assert res.output == ''
+
+    out = tmpdir.join('tmp.nc')
+    res = runner.invoke(__main__.cfgrib_cli, ['to_netcdf', TEST_DATA, '-o' + str(out), '-cCDS'])
 
     assert res.exit_code == 0
     assert res.output == ''
