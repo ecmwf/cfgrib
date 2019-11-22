e5970468aaac802e4210b83ed2927cf7690cfe14
Alessandro Amici <a.amici@bopen.eu>
2019-11-22 18:14:49 +0100

    Code style

--- cfgrib/dataset.py
+++ cfgrib/dataset.py
@@ -502,7 +502,10 @@ def build_variable_components(
 
     if 'time' in coord_vars and 'step' in coord_vars:
         # add the 'valid_time' secondary coordinate
-        dims, time_data = cfmessage.build_valid_time(coord_vars['time'].data, coord_vars['step'].data)
+        dims, time_data = cfmessage.build_valid_time(
+            coord_vars['time'].data,
+            coord_vars['step'].data,
+        )
         attrs = COORD_ATTRS['valid_time']
         coord_vars['valid_time'] = Variable(dimensions=dims, data=time_data, attributes=attrs)
 
