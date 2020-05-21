efe5e7e23100fd7be7c99cb01d7fba00af7c985b
Alessandro Amici <a.amici@bopen.eu>
2020-05-21 17:07:13 +0200

    Code style

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
     name='cfgrib',
     version=parse_version_from('cfgrib/__init__.py'),
     description='Python interface to map GRIB files to the NetCDF Common Data Model '
-                'following the CF Convention using ecCodes.',
+    'following the CF Convention using ecCodes.',
     long_description=read('README.rst') + read('CHANGELOG.rst'),
     author='European Centre for Medium-Range Weather Forecasts (ECMWF)',
     author_email='software.support@ecmwf.int',
@@ -48,19 +48,10 @@ setuptools.setup(
     url='https://github.com/ecmwf/cfgrib',
     packages=setuptools.find_packages(),
     include_package_data=True,
-    setup_requires=[
-        'pytest-runner',
-    ],
-    install_requires=[
-        'attrs>=19.2',
-        'cffi',
-        'click',
-        'numpy',
-    ],
+    setup_requires=['pytest-runner',],
+    install_requires=['attrs>=19.2', 'cffi', 'click', 'numpy',],
     python_requires='>=3.5',
-    extras_require={
-        'xarray': ['xarray>=0.12.0'],
-    },
+    extras_require={'xarray': ['xarray>=0.12.0'],},
     tests_require=[
         'dask[array]',
         'pytest',
@@ -84,9 +75,5 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: PyPy',
         'Operating System :: OS Independent',
     ],
-    entry_points={
-        'console_scripts': [
-            'cfgrib=cfgrib.__main__:cfgrib_cli',
-        ],
-    },
+    entry_points={'console_scripts': ['cfgrib=cfgrib.__main__:cfgrib_cli',],},
 )
