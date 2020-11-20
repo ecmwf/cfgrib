e0edda76ffe219fefe95956a706c6337c1f7185e
Alessandro Amici <a.amici@bopen.eu>
2020-11-20 16:19:33 +0100

    100% coverage of tests

--- tests/test_20_messages.py
+++ tests/test_20_messages.py
@@ -195,7 +195,7 @@ def test_FileIndex_from_indexpath_or_filestream(tmpdir):
 
 def test_FileIndex_errors():
     class MyMessage(messages.ComputedKeysMessage):
-        computed_keys = {"error_key": lambda m: 1 / 0}
+        computed_keys = {"error_key": lambda m: 1 / 0}  # pragma: no branch
 
     stream = messages.FileStream(TEST_DATA, message_class=MyMessage)
     res = messages.FileIndex.from_filestream(stream, ["paramId", "error_key"])
