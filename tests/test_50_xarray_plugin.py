9a22ae57c1222763c473668c21e189be7823c6f6
Aureliana Barghini <a.barghini@bopen.eu>
2021-02-23 16:32:46 +0100

    modify test data in tests xarray_plugin

--- tests/test_50_xarray_plugin.py
+++ tests/test_50_xarray_plugin.py
@@ -5,7 +5,7 @@ import pytest
 import xarray as xr
 
 SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), "sample-data")
-TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, "example.grib")
+TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, "regular_ll_sfc.grib")
 
 
 @pytest.mark.skipif(
@@ -20,17 +20,13 @@ def test_plugin():
 
 def test_xr_open_dataset():
     expected = {
-        "number": 2,
-        "time": 3,
-        "isobaricInhPa": 2,
-        "latitude": 3,
-        "longitude": 4,
+        "latitude": 37,
+        "longitude": 72,
     }
 
     ds = xr.open_dataset(TEST_DATA, engine="cfgrib")
     assert ds.dims == expected
-    assert list(ds.data_vars) == ["z", "t"]
-    assert ds["z"].min() == 12660.0
+    assert list(ds.data_vars) == ["skt"]
 
 
 @pytest.mark.skipif(
@@ -39,16 +35,12 @@ def test_xr_open_dataset():
 )
 def test_read():
     expected = {
-        "number": 2,
-        "time": 3,
-        "isobaricInhPa": 2,
-        "latitude": 3,
-        "longitude": 4,
+        "latitude": 37,
+        "longitude": 72,
     }
     import cfgrib.xarray_entrypoint
 
     opener = cfgrib.xarray_entrypoint.CfgribfBackendEntrypoint()
     ds = opener.open_dataset(TEST_DATA)
     assert ds.dims == expected
-    assert list(ds.data_vars) == ["z", "t"]
-    assert ds["z"].min() == 12660.0
+    assert list(ds.data_vars) == ["skt"]
