053d4ab459f44b5b236cdbfaed3ca17cbcf9c736
Alessandro Amici <a.amici@bopen.eu>
2021-08-02 17:57:22 +0200

    Improve mypy coverage.

--- cfgrib/abc.py
+++ cfgrib/abc.py
@@ -12,3 +12,7 @@ IndexTypeVar = T.TypeVar("IndexTypeVar", bound="Index")  # type: ignore
 Message = T.Mapping[str, T.Any]
 MutableMessage = T.MutableMapping[str, T.Any]
 Container = T.Mapping[ItemTypeVar, MessageTypeVar]
+
+
+class Index(T.Mapping[str, T.List[T.Any]], T.Generic[ItemTypeVar, MessageTypeVar]):
+    pass
