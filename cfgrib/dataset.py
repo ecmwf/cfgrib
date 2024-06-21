4baeb37f155ce097faef267b4573a61a141ff26e
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2024-06-21 08:33:10 +0100

    optional level encoding

--- cfgrib/dataset.py
+++ cfgrib/dataset.py
@@ -463,7 +463,8 @@ def encode_cf_first(data_var_attrs, encode_cf=("parameter", "time"), time_dims=(
             raise ValueError("time_dims %r not a subset of %r" % (time_dims, ALL_REF_TIME_KEYS))
     else:
         coords_map.extend(DATA_TIME_KEYS)
-    coords_map.extend(VERTICAL_KEYS)
+    if "level" in encode_cf:
+        coords_map.extend(VERTICAL_KEYS)
     coords_map.extend(SPECTRA_KEYS)
     return coords_map
 
